1be9aaa8a783165230ede17ab5d253be5c1a88d8
Parshukov Alexandr <parshukovae@yandex.ru>
2022-08-24 20:08:16 +0300

    исправил роут мувис

--- routes/movies.js
+++ routes/movies.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
-const validator = require('validator');
 const { addMoviesValidation, movieIdValidation } = require('../utils/validations');
-const BadRequestError = require('../errors/BadRequestError');
 
 const {
   getMovies,
@@ -14,6 +12,6 @@ router.get('/movies', getMovies);
 
 router.post('/movies', addMoviesValidation, addMovies);
 
-router.delete('/movies/:movieId',  movieIdValidation, deleteMovies);
+router.delete('/movies/:movieId', movieIdValidation, deleteMovies);
 
 module.exports = router;
